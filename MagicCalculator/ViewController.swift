f534fa13ade1e6c37201225af0540136b1d20439
jurilorenz <114692921+jurilorenz@users.noreply.github.com>
2023-07-05 16:12:01 +0200

    Initial Commit

---
+++ MagicCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicCalculator
+//
+//  Created by Juri Lorenz on 05.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
