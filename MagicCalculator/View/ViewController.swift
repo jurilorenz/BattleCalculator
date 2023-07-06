f25332e4b27d7cc084e465fbdbeb082caac701a0
jurilorenz <114692921+jurilorenz@users.noreply.github.com>
2023-07-06 13:35:40 +0200

    Added IBOutlet and IBActions.

---
+++ MagicCalculator/View/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  MagicCalculator
+//
+//  Created by Juri Lorenz on 05.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var isFinishedTypingNumber: Bool = true
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
+    private var calclator = CalculatorLogic()
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+        
+        isFinishedTypingNumber = true
+        
+        calclator.setNumber(displayValue)
+        
+        if let calcMethod = sender.currentTitle {
+            
+            if let result = calclator.calculate(symbol: calcMethod) {
+                displayValue = result
+            }
+        }
+        
+    }
+    
+
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
+    }
+}
+
+
